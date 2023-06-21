7c447b074b4260e56f0ecde8152c70083353d0ef
kpashindla29 <87865678+kpashindla29@users.noreply.github.com>
2023-06-21 10:52:19 +0100

    Update HomeController.java

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -3,12 +3,12 @@ package com.ab.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@RestController
 public class HomeController {
 	
 	@GetMapping
 	public String home() {
-		return "home";
+		return "Welcome";
 	}
 	
 	
