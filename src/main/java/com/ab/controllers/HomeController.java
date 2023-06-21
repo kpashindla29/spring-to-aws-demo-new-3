db5ab6a1fc1840902f00b60cc20fee452ffc9218
kpashindla29 <87865678+kpashindla29@users.noreply.github.com>
2023-06-21 10:56:48 +0100

    Update HomeController.java

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -1,6 +1,6 @@
 package com.ab.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
