bd12634894a171bf907cd9927d0754ca020051c1
kpashindla29 <87865678+kpashindla29@users.noreply.github.com>
2023-06-22 09:58:17 +0100

    Update HomeController.java

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RestController
 public class HomeController {
 	
-	@GetMapping
+	@GetMapping("/")
 	public String home() {
 		return "Welcome";
 	}
