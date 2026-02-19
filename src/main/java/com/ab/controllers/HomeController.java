156c2c57ce6e01558d05ec59f9bfc78b3f0e4e63
kpashindla29 <87865678+kpashindla29@users.noreply.github.com>
2026-02-19 09:59:28 +0000

    Add RequestMapping to HomeController

--- src/main/java/com/ab/controllers/HomeController.java
+++ src/main/java/com/ab/controllers/HomeController.java
@@ -2,8 +2,9 @@ package com.ab.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
+@RequestMapping("/api/vi")	
 public class HomeController {
 	
 	@GetMapping("/")
